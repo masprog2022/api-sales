ceb6cdebf4f870f916df43b3ff1248ab5b299053
masprog2022 <masprog2022@gmail.com>
2023-08-19 01:32:47 +0100

    :sparkles: feat: add metodo para listar todas vendas findAllPaged

--- src/main/java/me/masprogtechs/apisales/dto/SaleItemResponseDTO.java
+++ src/main/java/me/masprogtechs/apisales/dto/SaleItemResponseDTO.java
@@ -1,8 +1,12 @@
 package me.masprogtechs.apisales.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import me.masprogtechs.apisales.domain.entities.SaleItem;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 public class SaleItemResponseDTO {
@@ -11,4 +15,6 @@ public class SaleItemResponseDTO {
     private BigDecimal price;
     private int quantity;
     private BigDecimal subtotal;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/me/masprogtechs/apisales/dto/SaleResponseDTO.java
+++ src/main/java/me/masprogtechs/apisales/dto/SaleResponseDTO.java
@@ -1,10 +1,16 @@
 package me.masprogtechs.apisales.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import me.masprogtechs.apisales.domain.entities.SaleItem;
 import me.masprogtechs.apisales.domain.enums.PaymentEnum;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.LongConsumer;
 
 @Data
 public class SaleResponseDTO {
@@ -14,4 +20,7 @@ public class SaleResponseDTO {
     private BigDecimal amountPaid;
     private BigDecimal difference;
     private PaymentEnum payment;
+
+
+
 }
